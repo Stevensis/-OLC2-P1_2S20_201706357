44ffcee5eb519f57a3128b603b79219ba63906ac
Stevensis <aaronsishernandez@gmail.com>
2020-09-16 01:31:15 -0600

    Prueba 0.8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SisComponent } from './sis/sis.component';
+import { PruebaComponent } from './prueba/prueba.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SisComponent
+    SisComponent,
+    PruebaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
